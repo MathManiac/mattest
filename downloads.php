34868d4d350eb2ddef38dc23e216121b4bbf8a8a
Henrik Hansen <lianides@hotmail.com>
2019-10-10 10:28:09 +0200

    Startfilerne
    Lidt start filer med .pdf og .docx.
    Visning er på plads, men rod med .docx som tvangsdownloades

---
+++ downloads.php
@@ -0,0 +1,40 @@
+<?php include 'filesLogic.php';?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css">
+  <title>Download files</title>
+</head>
+<body>
+
+<table>
+<thead>
+    <th>ID</th>
+    <th>Filename</th>
+    <th>size (in mb)</th>
+    <th>Visning</th>
+    <th>Downloads</th>
+    <th>Action</th>
+    <th>Note</th>
+</thead>
+<tbody>
+  <?php foreach ($files as $file): ?>
+    <tr>
+      <td><?php echo $file['id']; ?></td>
+      <td><?php echo $file['name']; ?></td>
+      <td><?php echo floor($file['size'] / 1000) . ' KB'; ?></td>
+      <td><embed src="<?php echo $file['path'] ?>" type="application/pdf" width="100%" height="200px" /></td>
+      <td><?php echo $file['downloads']; ?></td>
+      <td><a href="downloads.php?file_id=<?php echo $file['id'] ?>">Download</a></td>
+        <td><?php $extension ?></td>
+    </tr>
+   
+  <?php endforeach;?>
+
+</tbody>
+</table>
+
+
+</body>
+</html>
